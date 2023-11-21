3ccd1d31406fa69879228710e2b88d6bcb96e6e5
Nastya <Nastya.k_2001@mail.ru>
2023-11-21 16:11:50 +0500

    added frontend

---
+++ index.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test1</title>
+    <link rel="stylesheet" href="frontend/style.css">
+    <script defer src="frontend/script.js"></script>
+    <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.7.1.min.js"></script>
+</head>
+<body>
+    <header>Test1</header>
+    <main>
+        <h1>Pяд чисел Фибоначи</h1>
+        <div class="fibonacci">
+            <input class="fibonacci__input" type="text">
+            <ul class="fibonacci__ul">
+                
+            </ul>
+        </div>
+        
+    </main>
+    <footer></footer>
+</body>
+</html>
