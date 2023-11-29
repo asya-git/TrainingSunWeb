9fdf18380f59453f0b25c067217a5eeff39220aa
Nastya <Nastya.k_2001@mail.ru>
2023-11-29 18:51:52 +0500

    Создан header, баннер
    index.php -> проектировка header'а и баннера по БЭМ
    style.css -> сделаны стили для header'а и баннера

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Stoff</title>
+</head>
+<body>
+    <div class="intro">
+        <!-- HEADER -->
+        <header class="intro__header header">
+            <div  class="header__logo">
+                <img height="37" width="180" class="header__img" src="img/header/logo.png" alt="лого">
+                <p class="header__text">Центр мебельных технологий</p>
+            </div>
+            <nav class="header__nav">
+                <ul class="header__menu-list">
+                    <li class="header__menu-item">
+                        <a href="#" class="header__menu-link">Шкафы-купе</a>
+                    </li>
+                    <li class="header__menu-item">
+                        <a href="#" class="header__menu-link">Торговая мебель</a>
+                    </li>
+                    <li class="header__menu-item">
+                        <a href="#" class="header__menu-link">Кухни</a>
+                    </li>
+                    <li class="header__menu-item">
+                        <a href="#" class="header__menu-link">Контакты</a>
+                    </li>
+                </ul>
+            </nav>
+
+            <a href="tel:+7345254-70-78" class="header__phone">+7 3452 54-70-78</a>
+            <button class="header__button">Оставить заявку</button>
+        
+        </header>
+
+        <!-- БАННЕР -->
+        <div class="intro__banner banner">
+            <h1 class="banner__title">Заказывайте кухню от производителя</h1>
+            <p class="banner__description">Высокое качество, профессиональный подход и креативные решения</p>
+            <p class="banner__subdescription">Работаем по Тюмени и Тюменской области</p>
+        </div>
+        <button class="intro__button">Оставить заявку</button>
+    </div>
+</body>
+</html>
